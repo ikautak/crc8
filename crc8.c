137192414fe66097e0246792ac436a09d574bc02
Katsuaki Oshio <katsuaki.oshio@gmail.com>
2022-07-24 00:03:52 +0900

    chore: use inttypes

--- crc8.c
+++ crc8.c
@@ -1,14 +1,14 @@
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-unsigned char crc8_lsb(unsigned char poly, unsigned char *data, int size) {
-  unsigned char crc = 0x00;
-  int bit;
+uint8_t crc8_lsb(uint8_t poly, uint8_t *data, size_t size) {
+  uint8_t crc = 0x00;
 
   while (size--) {
     crc ^= *data++;
-    for (bit = 0; bit < 8; bit++) {
+    for (int bit = 0; bit < 8; bit++) {
       if (crc & 0x01) {
         crc = (crc >> 1) ^ poly;
       } else {
@@ -20,13 +20,12 @@ unsigned char crc8_lsb(unsigned char poly, unsigned char *data, int size) {
   return crc;
 }
 
-unsigned char crc8_msb(unsigned char poly, unsigned char *data, int size) {
-  unsigned char crc = 0x00;
-  int bit;
+uint8_t crc8_msb(uint8_t poly, uint8_t *data, size_t size) {
+  uint8_t crc = 0x00;
 
   while (size--) {
     crc ^= *data++;
-    for (bit = 0; bit < 8; bit++) {
+    for (int bit = 0; bit < 8; bit++) {
       if (crc & 0x80) {
         crc = (crc << 1) ^ poly;
       } else {
@@ -40,17 +39,17 @@ unsigned char crc8_msb(unsigned char poly, unsigned char *data, int size) {
 
 int main(void) {
   char *data = "Hello World!!!";
-  unsigned char poly;
+  uint8_t poly;
 
   // x8 + x4 + x3 + x2 + 1 -> 0x1D
   poly = 0x1D;
-  printf("%02x\n", crc8_lsb(poly, data, strlen(data)));
-  printf("%02x\n", crc8_msb(poly, data, strlen(data)));
+  printf("%02x\n", crc8_lsb(poly, (uint8_t *)data, strlen(data)));
+  printf("%02x\n", crc8_msb(poly, (uint8_t *)data, strlen(data)));
 
   // x8 + x5 + x4 + 1 -> 0x31
   poly = 0x31;
-  printf("%02x\n", crc8_lsb(poly, data, strlen(data)));
-  printf("%02x\n", crc8_msb(poly, data, strlen(data)));
+  printf("%02x\n", crc8_lsb(poly, (uint8_t *)data, strlen(data)));
+  printf("%02x\n", crc8_msb(poly, (uint8_t *)data, strlen(data)));
 
   return EXIT_SUCCESS;
 }
